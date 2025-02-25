abee8fb15322a1361817e6ae62a6c9112001dbda
Zafer Mesut Bilen <wildones.zafer@gmail.com>
2025-02-26 02:46:57 +0300

    string edited

--- strings.py
+++ strings.py
@@ -1,17 +1,17 @@
 COMMANDS = """
 🚀 *Raspberry Pi Telegram Bot Komutları:*
 
-`/status` → Raspberry Pi’nin durumunu gösterir
+/status → Raspberry Pi’nin durumunu gösterir
 
-`/check_update` → Güncellemeleri kontrol eder  
+/check_update → Güncellemeleri kontrol eder  
 
-`/update` → Kodları GitHub’dan güncelleyip botu yeniden başlatır  
+/update → Kodları GitHub’dan güncelleyip botu yeniden başlatır  
 
-`/restart` → Raspberry Pi’yi yeniden başlatır  
+/restart → Raspberry Pi’yi yeniden başlatır  
 
-`/shutdown` → Raspberry Pi’yi kapatır  
+/shutdown → Raspberry Pi’yi kapatır  
 
-`/cpu_temp` → Raspberry Pi’nin CPU sıcaklığını döndürür
+/cpu_temp → Raspberry Pi’nin CPU sıcaklığını döndürür
 
-`/help` → Tüm komutları ve açıklamalarını gösterir  
+/help → Tüm komutları ve açıklamalarını gösterir  
 """
